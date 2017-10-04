a1996c7f594f39263165efc118325a895c10a5dc
mtso <mtso@tuta.io>
2017-10-03 23:10:52 -0700

    Fill in the blanks

--- src/koans/basics.sql
+++ src/koans/basics.sql
@@ -2,22 +2,29 @@
 SELECT 1
 
 -- Meditate on lower-case queries
-_____ 1
+select 1
 
 -- Meditate on selecting all columns from a table
 select * from book
 
 -- Meditate on selecting one column (title) from a table
-select _____ from book
+select [title] from book
 
 -- Meditate on selecting some columns (title, available) from a table
-select _____, _____ from book
+select title, available from book
 
 -- Meditate on adding a new book
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
+values('1Q84', 'Dystopian Fiction', 2009, 1)
 
 -- Meditate on adding two new books
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
-,('_____', '_____', _____, 1)
+values('title1', 'genre1', 2000, 1)
+,('title2', 'genre2', 2000, 1)
+
+/*
+insert into book(title, genre, year_published, available)
+SELECT 'title1', 'genre1', 1000, 0
+UNION
+SELECT 'title2', 'genre2', 2000, 1
+*/
